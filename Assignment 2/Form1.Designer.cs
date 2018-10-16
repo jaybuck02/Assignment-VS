9515c8c782adf7cbf66eabd47c58710b599d4639
RC0034657 <RC0034657@CRO-24-05>
2018-10-16 10:38:30 +0100

    Fully working series

--- Assignment 2/Form1.Designer.cs
+++ Assignment 2/Form1.Designer.cs
@@ -28,13 +28,353 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.r1text = new System.Windows.Forms.TextBox();
+            this.r2text = new System.Windows.Forms.TextBox();
+            this.r4text = new System.Windows.Forms.TextBox();
+            this.r5text = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.r3text = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.r2txt = new System.Windows.Forms.TextBox();
+            this.r3txt = new System.Windows.Forms.TextBox();
+            this.r4txt = new System.Windows.Forms.TextBox();
+            this.r1txt = new System.Windows.Forms.TextBox();
+            this.r5txt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.r1lbl = new System.Windows.Forms.Label();
+            this.r2lbl = new System.Windows.Forms.Label();
+            this.r3lbl = new System.Windows.Forms.Label();
+            this.r4lbl = new System.Windows.Forms.Label();
+            this.r5lbl = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // r1text
+            // 
+            this.r1text.Location = new System.Drawing.Point(269, 74);
+            this.r1text.Name = "r1text";
+            this.r1text.Size = new System.Drawing.Size(100, 20);
+            this.r1text.TabIndex = 0;
+            // 
+            // r2text
+            // 
+            this.r2text.Location = new System.Drawing.Point(451, 74);
+            this.r2text.Name = "r2text";
+            this.r2text.Size = new System.Drawing.Size(100, 20);
+            this.r2text.TabIndex = 1;
+            // 
+            // r4text
+            // 
+            this.r4text.Location = new System.Drawing.Point(451, 125);
+            this.r4text.Name = "r4text";
+            this.r4text.Size = new System.Drawing.Size(100, 20);
+            this.r4text.TabIndex = 3;
+            // 
+            // r5text
+            // 
+            this.r5text.Location = new System.Drawing.Point(269, 169);
+            this.r5text.Name = "r5text";
+            this.r5text.Size = new System.Drawing.Size(100, 20);
+            this.r5text.TabIndex = 4;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label12);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.r3text);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.r5text);
+            this.tabPage1.Controls.Add(this.r1text);
+            this.tabPage1.Controls.Add(this.r4text);
+            this.tabPage1.Controls.Add(this.r2text);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 424);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Series";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label11);
+            this.tabPage2.Controls.Add(this.r5lbl);
+            this.tabPage2.Controls.Add(this.r4lbl);
+            this.tabPage2.Controls.Add(this.r3lbl);
+            this.tabPage2.Controls.Add(this.r2lbl);
+            this.tabPage2.Controls.Add(this.r1lbl);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.r5txt);
+            this.tabPage2.Controls.Add(this.r1txt);
+            this.tabPage2.Controls.Add(this.r4txt);
+            this.tabPage2.Controls.Add(this.r3txt);
+            this.tabPage2.Controls.Add(this.r2txt);
+            this.tabPage2.Controls.Add(this.comboBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 424);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Parallel";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(212, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Resistor 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(396, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Resistor 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(212, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Resistor 3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(396, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Resistor 4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(212, 172);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Resistor 5";
+            // 
+            // r3text
+            // 
+            this.r3text.Location = new System.Drawing.Point(269, 122);
+            this.r3text.Name = "r3text";
+            this.r3text.Size = new System.Drawing.Size(100, 20);
+            this.r3text.TabIndex = 10;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "2 Resistors",
+            "3 Resistors",
+            "4 Resistors",
+            "5 Resistors"});
+            this.comboBox1.Location = new System.Drawing.Point(89, 62);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // r2txt
+            // 
+            this.r2txt.Location = new System.Drawing.Point(325, 88);
+            this.r2txt.Name = "r2txt";
+            this.r2txt.Size = new System.Drawing.Size(100, 20);
+            this.r2txt.TabIndex = 1;
+            // 
+            // r3txt
+            // 
+            this.r3txt.Location = new System.Drawing.Point(325, 115);
+            this.r3txt.Name = "r3txt";
+            this.r3txt.Size = new System.Drawing.Size(100, 20);
+            this.r3txt.TabIndex = 2;
+            // 
+            // r4txt
+            // 
+            this.r4txt.Location = new System.Drawing.Point(325, 142);
+            this.r4txt.Name = "r4txt";
+            this.r4txt.Size = new System.Drawing.Size(100, 20);
+            this.r4txt.TabIndex = 3;
+            // 
+            // r1txt
+            // 
+            this.r1txt.Location = new System.Drawing.Point(325, 62);
+            this.r1txt.Name = "r1txt";
+            this.r1txt.Size = new System.Drawing.Size(100, 20);
+            this.r1txt.TabIndex = 4;
+            // 
+            // r5txt
+            // 
+            this.r5txt.Location = new System.Drawing.Point(325, 169);
+            this.r5txt.Name = "r5txt";
+            this.r5txt.Size = new System.Drawing.Size(100, 20);
+            this.r5txt.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(451, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(89, 206);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Calculate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // r1lbl
+            // 
+            this.r1lbl.AutoSize = true;
+            this.r1lbl.Location = new System.Drawing.Point(251, 65);
+            this.r1lbl.Name = "r1lbl";
+            this.r1lbl.Size = new System.Drawing.Size(54, 13);
+            this.r1lbl.TabIndex = 7;
+            this.r1lbl.Text = "Resistor 1";
+            // 
+            // r2lbl
+            // 
+            this.r2lbl.AutoSize = true;
+            this.r2lbl.Location = new System.Drawing.Point(251, 91);
+            this.r2lbl.Name = "r2lbl";
+            this.r2lbl.Size = new System.Drawing.Size(54, 13);
+            this.r2lbl.TabIndex = 8;
+            this.r2lbl.Text = "Resistor 2";
+            // 
+            // r3lbl
+            // 
+            this.r3lbl.AutoSize = true;
+            this.r3lbl.Location = new System.Drawing.Point(251, 118);
+            this.r3lbl.Name = "r3lbl";
+            this.r3lbl.Size = new System.Drawing.Size(54, 13);
+            this.r3lbl.TabIndex = 9;
+            this.r3lbl.Text = "Resistor 3";
+            // 
+            // r4lbl
+            // 
+            this.r4lbl.AutoSize = true;
+            this.r4lbl.Location = new System.Drawing.Point(251, 145);
+            this.r4lbl.Name = "r4lbl";
+            this.r4lbl.Size = new System.Drawing.Size(54, 13);
+            this.r4lbl.TabIndex = 10;
+            this.r4lbl.Text = "Resistor 4";
+            // 
+            // r5lbl
+            // 
+            this.r5lbl.AutoSize = true;
+            this.r5lbl.Location = new System.Drawing.Point(251, 172);
+            this.r5lbl.Name = "r5lbl";
+            this.r5lbl.Size = new System.Drawing.Size(54, 13);
+            this.r5lbl.TabIndex = 11;
+            this.r5lbl.Text = "Resistor 5";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(86, 249);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(42, 13);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "Answer";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(451, 202);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(42, 13);
+            this.label12.TabIndex = 12;
+            this.label12.Text = "Answer";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox r1text;
+        private System.Windows.Forms.TextBox r2text;
+        private System.Windows.Forms.TextBox r4text;
+        private System.Windows.Forms.TextBox r5text;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox r3text;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox r5txt;
+        private System.Windows.Forms.TextBox r1txt;
+        private System.Windows.Forms.TextBox r4txt;
+        private System.Windows.Forms.TextBox r3txt;
+        private System.Windows.Forms.TextBox r2txt;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label r5lbl;
+        private System.Windows.Forms.Label r4lbl;
+        private System.Windows.Forms.Label r3lbl;
+        private System.Windows.Forms.Label r2lbl;
+        private System.Windows.Forms.Label r1lbl;
     }
 }
 
--- Assignment 2/Form1.cs
+++ Assignment 2/Form1.cs
@@ -16,5 +16,53 @@ namespace Assignment_2
         {
             InitializeComponent();
         }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                r3txt.Hide();
+                r3lbl.Hide();
+                r4txt.Hide();
+                r4lbl.Hide();
+                r5txt.Hide();
+                r5lbl.Hide();
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                r3txt.Show();
+                r3lbl.Show();
+
+                r4txt.Hide();
+                r4lbl.Hide();
+                r5txt.Hide();
+                r5lbl.Hide();
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                r3txt.Show();
+                r3lbl.Show();
+                r4txt.Show();
+                r4lbl.Show();
+
+                r5txt.Hide();
+                r5lbl.Hide();
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+                r3txt.Show();
+                r3lbl.Show();
+                r4txt.Show();
+                r4lbl.Show();
+                r5txt.Show();
+                r5lbl.Show();
+            }
+        
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
